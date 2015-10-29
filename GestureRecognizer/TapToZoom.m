67019e9e99a52c39b2a504ddc103482e77f6eb73
TuanTa <quangtuan.fsb@gmail.com>
2015-10-29 12:13:12 +0700

    ok

---
+++ GestureRecognizer/TapToZoom.m
@@ -0,0 +1,24 @@
+//
+//  TapToZoom.m
+//  UIScrollView+ToolBar
+//
+//  Created by TuanTa on 10/29/15.
+//  Copyright © 2015 Cuong Trinh. All rights reserved.
+//
+
+#import "TapToZoom.h"
+
+@interface TapToZoom ()
+
+@end
+
+@implementation TapToZoom
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+
+@end
